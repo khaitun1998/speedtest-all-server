de721bf714a4b13f3c14e5b276b75f4692f065a4
Duong Quang Khai <khaitun1998@gmail.com>
2019-06-16 20:41:37 +0700

    Add execution time each speedtest and the whole process

--- speedtest_all_server.py
+++ speedtest_all_server.py
@@ -2,10 +2,13 @@ import json
 import speedtest as st
 import csv
 import os, sys
+import time
+
+start_time = time.time()
 
 id_server = []
 
-header = 'id country name sponsor download(mbps) upload(mbps) ping(ms)'
+header = 'id country name sponsor download(mbps) upload(mbps) ping(ms) time_execute(s)'
 header = header.split()
 
 file = open(os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), 'result.csv')), 'w', newline='')
@@ -33,15 +36,18 @@ with open('server.json', encoding="utf8", newline='') as json_f:
     data = json.load(json_f)
     for d in data['server']:
         print(f'Testing server: {d["_name"]} in country: {d["_country"]}')
+        s_time = time.time()
         server_to_test = [int(d['_id'])]
         try:
             down ,up ,ping = speed_test_multi(server_to_test)
         except:
            print("error") 
-
-        result_str = f'{d["_id"]}_{d["_country"]}_{d["_name"]}_{d["_sponsor"]}_{round(down/1000000, 2)} Mbps_{round(up/1000000,2)} Mbps_{round(ping,2)} ms'
+        
+        result_str = f'{d["_id"]}_{d["_country"]}_{d["_name"]}_{d["_sponsor"]}_{round(down/1000000, 2)} Mbps_{round(up/1000000,2)} Mbps_{round(ping,2)} ms_{round(time.time() - s_time, 2)} s'
 
         file = open(os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), 'result.csv')), 'a', newline='')
         with file:
             writer = csv.writer(file)
             writer.writerow(result_str.split("_"))
+
+print(f'Total execution time: {time.time() - start_time}')
