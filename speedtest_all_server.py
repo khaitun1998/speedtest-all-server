0446bfca62e62dac3f9ddf365525331f843daa92
Duong Quang Khai <khaitun1998@gmail.com>
2019-12-26 13:30:49 +0000

    update using official Ookla Speedtest CLI instead of pip3 speedtest-cli

--- speedtest_all_server.py
+++ speedtest_all_server.py
@@ -1,5 +1,4 @@
 import json
-import speedtest as st
 import csv
 import os, sys
 import time
@@ -10,20 +9,20 @@ from optparse import OptionParser
 start_time = time.time()
 
 filename = "server_minimize.json"
-test_type = ""
+# test_type = ""
 id_server = []
 
-header = 'id country name sponsor download(mbps) upload(mbps) ping(ms) time_execute(s)'
+header = 'id time location serverName download(mbps) upload(mbps) ping(ms) jitter(ms) result_url time_execute(s)'
 header = header.split()
 
 parser = OptionParser()
 
-parser.add_option("-t", "--type", dest="test_type", default="multi")
+# parser.add_option("-t", "--type", dest="test_type", default="multi")
 parser.add_option("-l", "--list", dest="test_list", default="minimize")
 
 (options, args) = parser.parse_args()
 
-print(options.test_type)
+# print(options.test_type)
 
 if options.test_list == 'minimize':
     filename = 'server_minimize.json'
@@ -38,44 +37,48 @@ with file:
     writer = csv.writer(file)
     writer.writerow(header)
 
-def speed_test_multi(server):
-    s = st.Speedtest()
-    s.get_servers(server)
-    s.download()
-    s.upload()
-    res = s.results.dict()
-    return res["download"], res["upload"], res["ping"]
-
-def speed_test_single(server):
-    s = st.Speedtest()
-    s.get_servers(server)
-    s.download(threads=1)
-    s.upload(threads=1)
-    res = s.results.dict()
-    return res["download"], res["upload"], res["ping"]
+count = 0
 
 with open(filename, encoding="utf8", newline='') as json_f:
     data = json.load(json_f)
     for d in data['server']:
         print(f'Testing server: {d["_name"]} in country: {d["_country"]}')
         s_time = time.time()
-        server_to_test = [int(d['_id'])]
+        server_to_test = int(d['_id'])
             
+        result_str = ""
+        
         try:
-            if options.test_type == 'multi':
-                down ,up ,ping = speed_test_multi(server_to_test)
-            elif options.test_type == 'single':
-                down ,up ,ping = speed_test_single(server_to_test)
-            else:
-                raise Exception('Invalid Argument')
+            # if options.test_type == 'multi':
+            #     down ,up ,ping = speed_test_multi(server_to_test)
+            # elif options.test_type == 'single':
+            #     down ,up ,ping = speed_test_single(server_to_test)
+            
+            stream = os.popen(f'speedtest -f json -s {server_to_test}')
+            
+            tmp_result = json.loads(stream.read())
+            
+            download = tmp_result["download"]["bandwidth"]
+            upload = tmp_result["upload"]["bandwidth"]
+            ping = tmp_result["ping"]["latency"]
+            jitter = tmp_result["ping"]["jitter"]
+            timestamp = tmp_result["timestamp"]
+            result_url = tmp_result["result"]["url"]
+            location = f'{tmp_result["server"]["location"]} - {tmp_result["server"]["country"]}'
+            server_name = f'{tmp_result["server"]["name"]}'
+            
+            result_str = f'{d["_id"]}_{timestamp}_{location}_{server_name}_{round((download*8)/(1024*1024), 2)}_{round((upload*8)/(1024*1024),2)}_{round(ping,2)}_{round(jitter,2)}_{result_url}_{round(time.time() - s_time, 2)} s'
+            
         except Exception:
            traceback.print_exc() 
+           result_str = f'error'
         
-        result_str = f'{d["_id"]}_{d["_country"]}_{d["_name"]}_{d["_sponsor"]}_{round(down/1000000, 2)} Mbps_{round(up/1000000,2)} Mbps_{round(ping,2)} ms_{round(time.time() - s_time, 2)} s'
-
         file = open(os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), 'result.csv')), 'a', newline='')
+        count+=1
+        print(f'Number of tested server: {count}')
+        
         with file:
             writer = csv.writer(file)
             writer.writerow(result_str.split("_"))
 
-print(f'Total execution time: {time.time() - start_time}')
+print(f'Total execution time: {time.time() - start_time} s')
